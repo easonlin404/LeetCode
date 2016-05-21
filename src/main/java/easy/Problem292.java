fa2261095bb5796e5c9e1c85b80686175b5c6f1b
easonlin MacBook Air <easonlin404@gmail.com>
2016-05-21 13:53:18 +0800

    292. Nim Game

---
+++ src/main/java/easy/Problem292.java
@@ -0,0 +1,15 @@
+package easy;
+
+/**
+ * 292. Nim Game
+ * @author EasonLin
+ *
+ */
+public class Problem292 {
+  
+  public class Solution {
+    public boolean canWinNim(int n) {
+      return n % 4 != 0;
+    }
+  }
+}
